b616186f1482b474092f3111ba7d93d79a7cbce6
LowBudgetGameDev <148505424+LowBudgetGameDev@users.noreply.github.com>
2025-08-02 10:04:20 -0400

    Added a parallax background effect

---
+++ Assets/Scripts/ParallaxBackground.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    [SerializeField] private float parallaxEffectultiplier = 1f;
+
+    private Transform cameraTransform;
+
+    private Vector3 lastCameraPosition;
+
+    private float textureUnitSizeX;
+
+    private void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        lastCameraPosition = cameraTransform.position;
+
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Texture2D texture = sprite.texture;
+        textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+        transform.position += deltaMovement * parallaxEffectultiplier;
+        lastCameraPosition = cameraTransform.position;
+
+        if (cameraTransform.position.x - transform.position.x >= textureUnitSizeX)
+        {
+            float offsetX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
+
+            transform.position = new Vector3(cameraTransform.position.x, transform.position.y);
+        }
+    }
+}
